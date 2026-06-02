b84d40d4f387bc243a1afd82bea3e12daa4190d8
Edgar <n00423975@upn.pe>
2026-06-02 13:39:35 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int[,] mat = new int[5,7];
+Random rand = new Random();
+
+for (int f = 0; f < mat.GetLongLength(0); f++)
+{ for (int C = 0; C < mat.GetLongLength(1); C++)
+    { mat[f,C] = rand.Next(1, 10);
+    }
+}
+for (int f = 0; f < mat.GetLongLength(0); f++)
+{
+    for (int C = 0; C < mat.GetLongLength(1); C++)
+    {
+       Console.Write(" "+ mat[f,C] +", ");
+    }
+    Console.WriteLine();
+}
